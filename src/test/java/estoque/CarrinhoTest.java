3f65eb4695797a7d044d59f2a873b096a0b72334
SamuelLemosS <samuellemosesilva@gmail.com>
2023-11-01 23:24:35 -0300

    estoque

--- src/test/java/estoque/CarrinhoTest.java
+++ src/test/java/estoque/CarrinhoTest.java
@@ -2,39 +2,99 @@ package estoque;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mock;
+
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 import com.example.estoque.Carrinho;
 import com.example.estoque.Produto;
 
-public class CarrinhoTest{
+
+public class CarrinhoTest {
     private Carrinho carrinho;
+    private Produto racaoMock;
+    private Produto coleiraMock;
 
     @Before
     public void setUp() {
         carrinho = new Carrinho();
+        racaoMock = mock(Produto.class);
+        coleiraMock = mock(Produto.class);
+
+        when(racaoMock.getNome()).thenReturn("Racao");
+        when(racaoMock.getPreco()).thenReturn(10.0);
+
+        when(coleiraMock.getNome()).thenReturn("Coleira");
+        when(coleiraMock.getPreco()).thenReturn(5.0);
     }
 
     @Test
     public void testAdicionarProduto() {
-        Produto racao = Produto.RACAO;
-        Produto coleira = Produto.COLEIRA;
+        carrinho.adicionarProduto(racaoMock);
+        carrinho.adicionarProduto(coleiraMock);
 
-        carrinho.adicionarProduto(racao);
-        carrinho.adicionarProduto(coleira);
+        Produto[] produtosNoCarrinho = carrinho.produtosNoCarrinho();
+        assertEquals(2, produtosNoCarrinho.length);
 
-        assertEquals(2, carrinho.produtosNoCarrinho().length);
+        verify(racaoMock, times(1)).getNome();
+        verify(racaoMock, times(1)).getPreco(); 
+        verify(coleiraMock, times(1)).getNome(); 
+        verify(coleiraMock, times(1)).getPreco(); 
     }
 
     @Test
-    public void testCalcularPrecoTotal() {
-        Produto racao = Produto.RACAO;
-        Produto coleira = Produto.COLEIRA;
+    public void testRetirarProduto() {
+        carrinho.adicionarProduto(racaoMock);
+        carrinho.adicionarProduto(coleiraMock);
+        carrinho.removerProduto(coleiraMock);
 
-        carrinho.adicionarProduto(racao);
-        carrinho.adicionarProduto(coleira);
+        Produto[] produtosNoCarrinho = carrinho.produtosNoCarrinho();
+        assertEquals(1, produtosNoCarrinho.length);
+
+        verify(racaoMock, times(1)).getNome(); 
+        verify(racaoMock, times(1)).getPreco(); 
+    }
+
+    @Test
+    public void testRetirandoTodosProdutos() {
+        carrinho.adicionarProduto(racaoMock);
+        carrinho.adicionarProduto(coleiraMock);
+        carrinho.retirarTudo();
+
+        Produto[] produtosNoCarrinho = carrinho.produtosNoCarrinho();
+        assertEquals(0, produtosNoCarrinho.length);
+
+    }
+    
+    @Test
+    public void testCalcularPrecoTotal() {
+        carrinho.adicionarProduto(racaoMock);
+        carrinho.adicionarProduto(coleiraMock);
 
         double precoTotal = carrinho.precoTotal();
         assertEquals(15.0, precoTotal, 0.01);
+
+        verify(racaoMock, times(1)).getNome(); 
+        verify(racaoMock, times(1)).getPreco(); 
+        verify(coleiraMock, times(1)).getNome(); 
+        verify(coleiraMock, times(1)).getPreco(); 
+    }
+
+    @Test
+    public void testCalcularParcelas() {
+        carrinho.adicionarProduto(racaoMock);
+        carrinho.adicionarProduto(coleiraMock);
+
+        double valorParcela = carrinho.parcelar(3);
+        assertEquals(5.0, valorParcela, 0.01);
+
+        verify(racaoMock, times(1)).getNome(); 
+        verify(racaoMock, times(1)).getPreco(); 
+        verify(coleiraMock, times(1)).getNome(); 
+        verify(coleiraMock, times(1)).getPreco(); 
     }
 }
