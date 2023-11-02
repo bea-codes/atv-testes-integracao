bc6bf41343b78d52f663ee580ca72e3d868f804b
Gabriela Lima <gwabriel@gmail.com>
2023-11-01 21:24:46 -0300

    Atualização do atendimento

--- src/main/java/com/example/atendimento/Atendimento.java
+++ src/main/java/com/example/atendimento/Atendimento.java
@@ -5,15 +5,12 @@ import java.util.Date;
 public class Atendimento {
     private Date data;
     private String tipoAtendimento;
-    // outros atributos e métodos
-
-    // Construtor
+    
     public Atendimento(Date data, String tipoAtendimento) {
         this.data = data;
         this.tipoAtendimento = tipoAtendimento;
     }
 
-    // Getters e Setters
     public Date getData() {
         return data;
     }
